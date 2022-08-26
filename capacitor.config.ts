91484d0ccdc293e0b7957493ff499f38ea060961
Sebastian Hermosilla <se.hermosillaf@gmail.com>
2022-08-25 23:08:57 -0400

    Commit incial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'control_de_tareas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
