d6923191f37fdf431d1ea6bdf01848acb221a5df
Sebastian Hermosilla <se.hermosillaf@gmail.com>
2022-08-28 03:06:52 -0400

    Fix: Corrige estilos en login e implementa funcion para iniciar sesión

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +9,27 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
   constructor(private router: Router) {}
-
   ngOnInit() {}
   nombre: any;
   email: any;
   password: any;
   confirm_password: any;
 
+  validarCamposVacios() {}
+
   iniciarSesion() {
-    console.log(this.nombre);
-    this.router.navigate(['/home']);
+    console.log(this.nombre, this.email, this.password, this.confirm_password);
+    if (this.password === this.confirm_password) {
+      this.router.navigate(['/home']);
+    } else {
+      Swal.fire({
+        title: 'Error',
+        text: 'Las contraseñas no coinciden',
+        icon: 'error',
+        heightAuto: false,
+      });
+    }
   }
+
+  validarEmail() {}
 }
