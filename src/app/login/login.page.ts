83bbb3c0162503841f4a05e1d958f70bcb15ba28
Sebastian Hermosilla Ferreira <se.hermosillaf@gmail.com>
2022-08-30 18:49:31 -0400

    Implementa funcion para verificar si el correo tiene el formato correcto con expresiones regulares

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { Validators } from '@angular/forms';
 //import { AuthService } from '../auth.service';
 import Swal from 'sweetalert2';
 
@@ -26,9 +27,24 @@ export class LoginPage implements OnInit {
 
   //   loading.present();
   // }
+  //TODO: verificar si esta bien la expresión regular
+  validarEmail(email): any {
+    const pattern = new RegExp('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\');
+    let result = pattern.test(email);
+    return result;
+  }
 
   onSubmit(form: NgForm) {
     console.log(form.value);
-    this.router.navigate(['/home']);
+    if (this.validarEmail(this.email)) {
+      this.router.navigate(['/home']);
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'El formato del correo no es correcto!',
+        heightAuto: false, // resuelve problema con ionic
+      });
+    }
   }
 }
