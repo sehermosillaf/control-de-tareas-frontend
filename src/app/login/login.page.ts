58635e99120666d4680f3d9427fa05aa5875db48
Sebastian Hermosilla Ferreira <se.hermosillaf@gmail.com>
2022-08-30 18:04:09 -0400

    Fix: Corrige layout de botones para la web y mobile

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 //import { AuthService } from '../auth.service';
 import Swal from 'sweetalert2';
 
@@ -10,12 +11,21 @@ import Swal from 'sweetalert2';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private loadingCtrl: LoadingController) {}
   ngOnInit() {}
   name: any;
   email: any;
   password: any;
   confirm_password: any;
+
+  // async showLoading() {
+  //   const loading = await this.loadingCtrl.create({
+  //     message: 'Dismissing after 3 seconds...',
+  //     duration: 3000,
+  //   });
+
+  //   loading.present();
+  // }
 
   onSubmit(form: NgForm) {
     console.log(form.value);
