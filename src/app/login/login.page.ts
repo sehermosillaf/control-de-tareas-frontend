e483d44675e4e5ddb4f6d9d012d77653e68a990a
Sebastian Hermosilla Ferreira <se.hermosillaf@gmail.com>
2022-08-30 16:52:37 -0400

    Fix: Agrega form y trabaja con NgModel

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+//import { AuthService } from '../auth.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,61 +17,8 @@ export class LoginPage implements OnInit {
   password: any;
   confirm_password: any;
 
-  //valida campos vacios
-  //TODO: Considerar refactorizar
-  validarCamposVacios() {
-    if (
-      this.name > 0 ||
-      this.email > 0 ||
-      this.password > 0 ||
-      this.confirm_password > 0
-    ) {
-      return true;
-    } else {
-      Swal.fire({
-        title: 'Error',
-        text: 'Todos los campos deben ser ingresados',
-        icon: 'error',
-        heightAuto: false,
-      });
-    }
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    this.router.navigate(['/home']);
   }
-
-  validarPassword() {
-    if (this.password === this.confirm_password) {
-      return true;
-    } else {
-      Swal.fire({
-        title: 'Error',
-        text: 'Las contraseñas no coinciden',
-        icon: 'error',
-        heightAuto: false,
-      });
-    }
-  }
-  //TODO: considerar refactorizar
-  iniciarSesion() {
-    if (this.validarCamposVacios() && this.validarPassword()) {
-      this.router.navigate(['/home']);
-    }
-  }
-
-  /* TODO: Buscar formas de validar formularios en Angular
-  validarEmail() {
-    let formatoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(
-      String(this.email).toLowerCase()
-    );
-
-    if (this.email.match(formatoEmail)) {
-      return true;
-    } else {
-      Swal.fire({
-        title: 'Error',
-        text: 'Formato de correo no valido',
-        icon: 'error',
-        heightAuto: false,
-      });
-    }
-  }
-  */
 }
