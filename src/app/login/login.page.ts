f3b0d22246079212cbf1d5c918ea6b4a04306bfa
Sebastian Hermosilla Ferreira <se.hermosillaf@gmail.com>
2022-08-30 21:49:00 -0400

    fix: Implementa funcion para verificar formato de correo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -14,37 +14,35 @@ import Swal from 'sweetalert2';
 export class LoginPage implements OnInit {
   constructor(private router: Router, private loadingCtrl: LoadingController) {}
   ngOnInit() {}
-  name: any;
-  email: any;
+  name: string;
+  email: string;
   password: any;
   confirm_password: any;
 
-  // async showLoading() {
-  //   const loading = await this.loadingCtrl.create({
-  //     message: 'Dismissing after 3 seconds...',
-  //     duration: 3000,
-  //   });
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Dismissing after 3 seconds...',
+      duration: 3000,
+    });
 
-  //   loading.present();
-  // }
-  //TODO: verificar si esta bien la expresión regular
-  validarEmail(email): any {
-    const pattern = new RegExp('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\');
-    let result = pattern.test(email);
-    return result;
+    loading.present();
   }
-
+  //TODO: verificar si esta bien la expresión regular
+  validarEmail(email) {
+    const regex = new RegExp('^(.+)@(.+)$', 'i');
+    let result = regex.test(email);
+    return result
+      ? this.router.navigateByUrl('home')
+      : Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'El formato del correo no es correcto!',
+          heightAuto: false, // resuelve problema con ionic
+        });
+  }
   onSubmit(form: NgForm) {
-    console.log(form.value);
-    if (this.validarEmail(this.email)) {
-      this.router.navigate(['/home']);
+    if (this.validarEmail(form.value['email'])) {
     } else {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'El formato del correo no es correcto!',
-        heightAuto: false, // resuelve problema con ionic
-      });
     }
   }
 }
